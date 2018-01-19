0da30eff98862d9d7f4923eb0c71d7716a09288c
Tony Lian <1040424979@qq.com>
2018-01-19 11:35:30 +0800

    Add StartAudioContext

--- src/tone.js
+++ src/tone.js
@@ -1,5 +1,7 @@
+let StartAudioContext = require('StartAudioContext');
 let Tone = require("Tone");//TODO: Update to ES6 when available
 
+StartAudioContext(Tone.context, "#root");
 export function newSynth(){//create a simple synth
   let synth = new Tone.Synth().toMaster();
   return synth;
